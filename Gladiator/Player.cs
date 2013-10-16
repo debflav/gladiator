64f37dfdc89c475da8f201e543640abeff175775
debflav <flavien@PC-Flavien.local>
2013-10-16 15:55:02 +0200

    Implementation Player&Team

--- Gladiator/Player.cs
+++ Gladiator/Player.cs
@@ -1,25 +1,76 @@
 using System;
+using System.Collections.Generic;
 
 namespace Gladiator
 {
 	public class Player
 	{
-		private string _name;
-		public string Name {
-			get { return this._name; }
-			set { this._name = value; }
+		private string _lastname;
+		public string Lastname {
+			get { return this._lastname; }
+			set { this._lastname = value; }
 		}
 
-		private string _prenom;
-		public string Prenom {
-			get { return this._prenom; }
-			set { this._prenom = value; }
+		private string _firstname;
+		public string Firstname {
+			get { return this._firstname; }
+			set { this._firstname = value; }
+		}
+
+		private string _pseudo;
+		public string Pseudo {
+			get { return this._pseudo; }
+			set { this._pseudo = value; }
+		}
+
+		private DateTime _dateInscription;
+		public DateTime DateInscription {
+			get { return this._dateInscription; }
+			set { this._dateInscription = value; }
+		}
+
+		private List<Team> _team = new List<Team>(); 
+		public List<Team> Team {
+			get { return this._team; }
 		}
 
 
-
-		public Player ()
+		public Player (string p_lastname, string p_firstname, string p_pseudo, DateTime p_dateInscription)
 		{
+			this.Lastname 			= p_lastname;
+			this.Firstname 			= p_firstname;
+			this.Pseudo 			= p_pseudo;
+			this.DateInscription 	= p_dateInscription;
+		}
+
+		/**
+		 * Ajouter une equipe au joueur.
+		 * Chaque joueur peut créer jusqu'à 5 équipes de gladiateurs.
+		 * 
+		 */
+		public void ajouterEquipe(Team p_team)
+		{
+			// Compte le nombre d'équipe actuel pour le joueur
+			int teamNumber = _team.Count;
+
+			if(teamNumber == 5) {
+				throw new Exception ("Limite du nombre d'équipe fixé à cinq");
+			}
+
+			// Insertion de l'équipe dans la collection
+			try {
+				this._team.Add (p_team);
+			} catch(Exception) {
+				throw new Exception ("Something wrong append.");
+			}
+
+			// Affichage des équipes
+			Console.WriteLine( "Les équipes sont:");
+
+			foreach (Team b_row in _team) {
+				Console.WriteLine( b_row.TeamName);
+			}
+			Console.WriteLine( "-------------------------------");
 		}
 	}
 }
--- Gladiator/Team.cs
+++ Gladiator/Team.cs
@@ -1,12 +1,93 @@
 using System;
+using System.Collections.Generic;
 
 namespace Gladiator
 {
 	public class Team
 	{
-		public Team ()
-		{
+
+		private static int _teamNumber = 1;
+		public static int TeamNumber {
+			get { return Team._teamNumber; }
+			set { Team._teamNumber = value; }
 		}
+
+		private string _teamName;
+		public string TeamName {
+			get { return this._teamName; }
+			set { this._teamName = value; }
+		}
+
+		private string _description;
+		public string Description {
+			get { return this._description; }
+			set { this._description = value; }
+		}
+
+		private int _winNumber;
+		public int WinNumber {
+			get { return this._winNumber; }
+			set { this._winNumber = value; }
+		}
+
+		private int _lostNumber;
+		public int LostNumber {
+			get { return this._lostNumber; }
+			set { this._lostNumber = value; }
+		}
+		
+		private int _matchNumber;
+		public int MatchNumber {
+			get { return this._matchNumber; }
+			set { this._matchNumber = value; }
+		}
+		
+		private bool _inGame;
+		public bool InGame {
+			get { return this._inGame; }
+			set { this._inGame = value; }
+		}
+
+
+		/**
+		 * Crée une équipe à l'instanciation de la classe
+		 */
+		public Team (string p_teamName, string p_description)
+		{
+			this.TeamName 			= p_teamName;
+			this.Description 		= p_description;
+
+			// Incremente le numéro de l'equipe
+			Team._teamNumber++;
+		}
+
+		/**
+		 * 
+		 * 
+		 */
+		public bool addGladiator()
+		{
+
+		}
+
+		/**
+		 * 
+		 * 
+		 */
+		public bool deleteGladiator()
+		{
+
+		}
+
+		/**
+		 * 
+		 * 
+		 */
+		public double getPercentVictoy()
+		{
+
+		}
+
 	}
 }
 
