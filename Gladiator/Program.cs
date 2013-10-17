c600c88842a3d5db28716bd141b6a6927f27c7d5
debflav <flavien@PC-Flavien.local>
2013-10-17 09:35:51 +0200

    merge

--- Gladiator/Program.cs
+++ Gladiator/Program.cs
@@ -20,6 +20,28 @@ namespace Gladiator
 			Console.WriteLine (MaLance.Name);
 			Console.WriteLine (monEpee.Name);
 
+			/* Initialisation Joueurs */
+			DateTime date1 = new DateTime(2010, 1, 18);
+			DateTime date2 = new DateTime(2010, 5, 16);
+
+			Player player1 = new Player ("Michel", "Jean", "JeanMich", date1);
+			Player player2 = new Player ("Toto", "Toto", "Toto", date2);
+
+			//Console.WriteLine (player1.Pseudo);
+			//Console.WriteLine (player2.Lastname);
+
+			/* Initialisation equipes */
+			Team team1 = new Team ("Les cougars", "On est des dingues !!!");
+			Team team2 = new Team ("Les tigres", "Tigrou est notre ami");
+
+			player1.ajouterEquipe (team1);
+			player1.ajouterEquipe (team2);
+
+			/* Ajout d'un gladiateur */
+			//Gladiator.
+
+			/* Ajout de gladiateurs à une équipe */
+			team1.addGladiator (team1);
 		}
 	}
 }
