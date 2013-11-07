0970a0a092098e6ba49b7ebe2ad6ef1458939d79
Aurelien <aureliendumont@macbook-pro.etudiants.loc>
2013-11-07 13:12:29 +0100

    jeu d'essai

--- Gladiator/Program.cs
+++ Gladiator/Program.cs
@@ -14,163 +14,174 @@ namespace Gladiator
 
 			Alert.initConsole ();
 
-			/* Création des joueurs */
+
+			/*
+			 * +--------------------------+
+			 * |	Jeu de test			  |
+			 * +--------------------------+
+			/* 
+
+			//Création des joueurs */
 			Player player1 = new Player ("Michel", "Jean", "JeanMich");
 			Player player2 = new Player ("Toto", "Toto", "Toto");
 			Player player3 = new Player ("Tata", "Tata", "Tata");
 			Player player4 = new Player ("Tutu", "Tutu", "Tutu");
+			Player player5 = new Player ("Tata", "Tata", "Tata");
+			Player player6 = new Player ("Tutu", "Tutu", "Tutu");
 
-			Gladiator gladiator1 = new Gladiator ("Zorg");
+			//-------------------------
+			// Team 1 //////////
+			Team team1 = new Team ("Les Molks", "On est des dingues !!!", player1);
+			team1.WinNumber = 9;
+			team1.MatchPlayed = 20;
+
+			// Team 1 : Gladiateur ////////
+			Gladiator gladiator1 = new Gladiator ("Molki");
 			gladiator1.addEquipment (new Dagger());
 			gladiator1.addEquipment (new LargeShield());
 
-			Gladiator gladiator2 = new Gladiator ("molk");
+			Gladiator gladiator2 = new Gladiator ("Molka");
 			gladiator2.addEquipment (new Net ());
 			gladiator2.addEquipment (new Helmet ());
 
-			Gladiator gladiator3 = new Gladiator ("Blop");
+			Gladiator gladiator3 = new Gladiator ("Molko");
 			gladiator3.addEquipment (new Sword());
 			gladiator3.addEquipment (new SmallShield());
 
-			Gladiator gladiator4 = new Gladiator ("plup");
-			gladiator4.addEquipment (new Dagger ());
-			gladiator4.addEquipment (new Net ());
-
-			Gladiator gladiator5 = new Gladiator ("blip");
-			gladiator5.addEquipment (new Spear());
-			gladiator5.addEquipment (new Helmet());
-
-			Gladiator gladiator6 = new Gladiator ("Aey");
-			gladiator6.addEquipment (new Trident ());
-			gladiator6.addEquipment (new Net ());
-
-			Gladiator gladiator7 = new Gladiator ("mechant");
-			gladiator7.addEquipment (new Dagger());
-			gladiator7.addEquipment (new LargeShield());
-
-			Gladiator gladiator8 = new Gladiator ("gentil");
-			gladiator8.addEquipment (new Sword ());
-			gladiator8.addEquipment (new Net ());
-
-			Gladiator gladiator9 = new Gladiator ("Captaine");
-			gladiator9.addEquipment (new Dagger());
-			gladiator9.addEquipment (new SmallShield());
-			gladiator9.addEquipment (new Helmet());
-
-			Gladiator gladiator10 = new Gladiator ("hulk");
-			gladiator10.addEquipment (new Sword ());
-			gladiator10.addEquipment (new Helmet ());
-
-			Gladiator gladiator11 = new Gladiator ("spiderman");
-			gladiator10.addEquipment (new Sword ());
-			gladiator10.addEquipment (new Helmet ());
-
-			Gladiator gladiator12 = new Gladiator ("batman");
-			gladiator10.addEquipment (new Sword ());
-			gladiator10.addEquipment (new Net ());
-
-			/* Initialisation equipes */
-			Team team1 = new Team ("Les cougars", "On est des dingues !!!", player1);
-			Team team2 = new Team ("Les tigres", "Tigrou est notre ami", player1);
-			Team team3 = new Team ("Panpan", "Cartouche !!!", player1);
-			//Team team4 = new Team ("Grincheux", "Les septs nains", player1);
-			//Team team5 = new Team ("La belle & la bete", "humpf", player1);
-	
-			Team team5 = new Team ("Angora", "On est des dingues !!!", player2);
-			Team team6 = new Team ("Chaton", "Tigrou est notre ami", player2);
-			Team team7 = new Team ("Poulette", "Cartouche !!!", player2);
-			//Team team8 = new Team ("chiot", "Les septs nains", player2);
-			//Team team9 = new Team ("tigrette", "humpf", player2);
-
-			Team team10 = new Team ("satan", "On est des dingues !!!", player3);
-			Team team11 = new Team ("mephistos", "Tigrou est notre ami", player3);
-			Team team12 = new Team ("metalica", "Cartouche !!!", player3);
-			//Team team13 = new Team ("666", "Les septs nains", player3);
-			//Team team14 = new Team ("Shetan", "humpf", player3);
-
-			Team team15 = new Team ("Mac", "On est des dingues !!!", player4);
-			Team team16 = new Team ("Linux", "Tigrou est notre ami", player4);
-			Team team17 = new Team ("PC", "Cartouche !!!", player4);
-			//Team team18 = new Team ("IBM", "Les septs nains", player4);
-			//Team team19 = new Team ("Amstrade", "humpf", player4);
-
-			//player2.showTeam ();
-			team1.WinNumber = 9;
-			team1.MatchPlayed = 20;
-			team2.WinNumber = 10;
-			team2.MatchPlayed = 11;
-			team3.WinNumber = 12;
-			team3.MatchPlayed = 26;
-			team5.WinNumber = 1;
-			team5.MatchPlayed = 12;
-			team6.WinNumber = 6;
-			team6.MatchPlayed = 20;
-			team7.WinNumber = 2;
-			team7.MatchPlayed = 12;
-			team10.WinNumber = 10;
-			team10.MatchPlayed = 10;
-			team11.WinNumber = 12;
-			team11.MatchPlayed = 16;
-			team12.WinNumber = 1;
-			team12.MatchPlayed = 9;
-			team15.WinNumber = 6;
-			team15.MatchPlayed = 20;
-			team16.WinNumber = 1;
-			team16.MatchPlayed = 100;
-			team17.WinNumber = 19;
-			team17.MatchPlayed = 20;
-
-			/* Add Gladiator to team */
-			// Player 1
 			team1.addGladiator (gladiator1);
 			team1.addGladiator (gladiator2);
 			team1.addGladiator (gladiator3);
-			/*team2.addGladiator (gladiator4);
+
+			//-------------------------
+			// Team 2 /////////
+			Team team2 = new Team ("Les Bisounours", "Tigrou est notre ami", player2);
+			team2.WinNumber = 10;
+			team2.MatchPlayed = 11;
+
+			// Team 2 : Gladiateur ////////
+			Gladiator gladiator4 = new Gladiator ("Toutdou");
+			gladiator4.addEquipment (new Dagger ());
+			gladiator4.addEquipment (new Net ());
+
+			Gladiator gladiator5 = new Gladiator ("Bisou");
+			gladiator5.addEquipment (new Spear());
+			gladiator5.addEquipment (new Helmet());
+
+			Gladiator gladiator6 = new Gladiator ("Calin");
+			gladiator6.addEquipment (new Trident ());
+			gladiator6.addEquipment (new Net ());
+
+			team2.addGladiator (gladiator4);
 			team2.addGladiator (gladiator5);
 			team2.addGladiator (gladiator6);
+
+			//-------------------------
+			// Team 3 /////////
+			Team team3 = new Team ("Les sombres", "Cartouche !!!", player3);
+			team3.WinNumber = 12;
+			team3.MatchPlayed = 26;
+
+			// Team 3 : Gladiateur ////////
+			Gladiator gladiator7 = new Gladiator ("Darkness");
+			gladiator7.addEquipment (new Dagger());
+			gladiator7.addEquipment (new LargeShield());
+
+			Gladiator gladiator8 = new Gladiator ("Noir");
+			gladiator8.addEquipment (new Sword ());
+			gladiator8.addEquipment (new Net ());
+
+			Gladiator gladiator9 = new Gladiator ("Ombre");
+			gladiator9.addEquipment (new Dagger());
+			gladiator9.addEquipment (new Helmet());
+
 			team3.addGladiator (gladiator7);
 			team3.addGladiator (gladiator8);
-			team3.addGladiator (gladiator9);*/
+			team3.addGladiator (gladiator9);
 
-			// Player 2
-			team5.addGladiator (gladiator4);
-			team5.addGladiator (gladiator5);
-			team5.addGladiator (gladiator6);
-			/*team6.addGladiator (gladiator3);
-			team6.addGladiator (gladiator4);
-			team6.addGladiator (gladiator5);
-			team7.addGladiator (gladiator6);
-			team7.addGladiator (gladiator7);
-			team7.addGladiator (gladiator8);*/
+			//-------------------------
+			// Team 4 /////////
+			Team team4 = new Team ("Les septs nains", "Bimm !!", player4);
+			team4.WinNumber = 13;
+			team4.MatchPlayed = 12;
 
-			// Player 3
-			team10.addGladiator (gladiator7);
-			team10.addGladiator (gladiator8);
-			team10.addGladiator (gladiator9);
-			/*team11.addGladiator (gladiator2);
-			team11.addGladiator (gladiator3);
-			team11.addGladiator (gladiator4);
-			team12.addGladiator (gladiator5);
-			team12.addGladiator (gladiator6);
-			team12.addGladiator (gladiator7);*/
+			// Team 4 : Gladiateur ////////
+			Gladiator gladiator10 = new Gladiator ("Grincheu");
+			gladiator10.addEquipment (new Sword ());
+			gladiator10.addEquipment (new Helmet ());
 
-			// Player 4
-			/*team15.addGladiator (gladiator8);
-			team15.addGladiator (gladiator9);
-			team15.addGladiator (gladiator10);
-			team16.addGladiator (gladiator1);
-			team16.addGladiator (gladiator2);
-			team16.addGladiator (gladiator3);*/
-			team17.addGladiator (gladiator10);
-			team17.addGladiator (gladiator11);
-			team17.addGladiator (gladiator12);
+			Gladiator gladiator11 = new Gladiator ("Dormeur");
+			gladiator11.addEquipment (new Dagger());
+			gladiator11.addEquipment (new LargeShield());
+
+			Gladiator gladiator12 = new Gladiator ("Timide");
+			gladiator12.addEquipment (new Sword ());
+			gladiator12.addEquipment (new Net ());
+
+			team4.addGladiator (gladiator10);
+			team4.addGladiator (gladiator11);
+			team4.addGladiator (gladiator12);
+
+			//-------------------------
+			// Team 5 /////////
+			Team team5 = new Team ("Les Animaux", "On est des dingues !!!", player5);
+			team5.WinNumber = 1;
+			team5.MatchPlayed = 12;
+
+			// Team 5 : Gladiateur ////////
+			Gladiator gladiator13 = new Gladiator ("Dauphin");
+			gladiator13.addEquipment (new Sword ());
+			gladiator13.addEquipment (new Net ());
+
+			Gladiator gladiator14 = new Gladiator ("Cheval");
+			gladiator14.addEquipment (new Dagger ());
+			gladiator14.addEquipment (new Helmet ());
+
+			Gladiator gladiator15 = new Gladiator ("Chaton");
+			gladiator15.addEquipment (new Sword ());
+			gladiator15.addEquipment (new Net ());
+
+			team5.addGladiator (gladiator13);
+			team5.addGladiator (gladiator14);
+			team5.addGladiator (gladiator15);
+
+			//-------------------------
+			// Team 6 /////////
+			Team team6 = new Team ("les Super heros", "Tigrou est notre ami", player6);
+			team6.WinNumber = 6;
+			team6.MatchPlayed = 20;
+
+			// Team 6 : Gladiateur ////////
+			Gladiator gladiator16 = new Gladiator ("Hulk");
+			gladiator16.addEquipment (new Dagger ());
+			gladiator16.addEquipment (new Helmet ());
+
+			Gladiator gladiator17 = new Gladiator ("Superman");
+			gladiator17.addEquipment (new Trident ());
+			gladiator17.addEquipment (new SmallShield ());
+
+			Gladiator gladiator18 = new Gladiator ("Batman");
+			gladiator18.addEquipment (new Sword ());
+			gladiator18.addEquipment (new Net ());
+
+			team6.addGladiator (gladiator16);
+			team6.addGladiator (gladiator17);
+			team6.addGladiator (gladiator18);
+
+
+			/*
+			 * +--------------------------+
+			 * |	Combat	     		  |
+			 * +--------------------------+
+			/* 
 
 			/*	Ajouter une team au combat */
 			Fight fight = new Fight ();
 			fight.addTeamToFight(team1);
+			fight.addTeamToFight(team2);
+			fight.addTeamToFight(team3);
+			fight.addTeamToFight(team4);
 			fight.addTeamToFight(team5);
-			fight.addTeamToFight(team10);
-			fight.addTeamToFight(team17);
+			fight.addTeamToFight(team6);
 			fight.initializeFight ();
 
 			// test Duel
